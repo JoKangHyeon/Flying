08a6e7c7ed9655621a68d3133523fdeb4f07bdd7
JoKangHyeon <jokh980324@naver.com>
2017-03-19 21:11:19 +0900

    Update ArrowGuid.cs

--- Assets/Script/Script/ArrowGuid.cs
+++ Assets/Script/Script/ArrowGuid.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class ArrowGuid : MonoBehaviour {
 
 	
-	
+	//Test
 	// Update is called once per frame
 	void Update () {
         transform.position = GameObject.Find("FlyingObject").GetComponent<Transform>().position;
